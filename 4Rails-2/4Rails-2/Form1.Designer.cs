4ff3e073f9e6bcd671bf7ddf722d228a688039ea
kevin hofstede <kevin_hofstede@live.nl>
2016-03-30 15:01:07 +0200

    je weet

--- 4Rails-2/4Rails-2/Form1.Designer.cs
+++ 4Rails-2/4Rails-2/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblGay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGay
+            // 
+            this.lblGay.AutoSize = true;
+            this.lblGay.Location = new System.Drawing.Point(186, 140);
+            this.lblGay.Name = "lblGay";
+            this.lblGay.Size = new System.Drawing.Size(132, 17);
+            this.lblGay.TabIndex = 0;
+            this.lblGay.Text = "BART IS UBERGAY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 387);
+            this.Controls.Add(this.lblGay);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblGay;
     }
 }
 
