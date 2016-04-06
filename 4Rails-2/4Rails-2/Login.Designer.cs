5168bc5f4b97a25b45163bb90eed62d88ed3baa3
Billy Trang <b.trang@student.fontys.nl>
2016-04-06 14:52:07 +0200

    Login shit v2

--- 4Rails-2/4Rails-2/Login.Designer.cs
+++ 4Rails-2/4Rails-2/Login.Designer.cs
@@ -35,47 +35,50 @@
             this.tbWachtwoord = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.chbRemember = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // lblGebruikersnaam
             // 
             this.lblGebruikersnaam.AutoSize = true;
-            this.lblGebruikersnaam.Location = new System.Drawing.Point(58, 157);
+            this.lblGebruikersnaam.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGebruikersnaam.Location = new System.Drawing.Point(44, 119);
             this.lblGebruikersnaam.Name = "lblGebruikersnaam";
-            this.lblGebruikersnaam.Size = new System.Drawing.Size(113, 17);
+            this.lblGebruikersnaam.Size = new System.Drawing.Size(97, 24);
             this.lblGebruikersnaam.TabIndex = 0;
-            this.lblGebruikersnaam.Text = "Gebruikersnaam";
+            this.lblGebruikersnaam.Text = "Username";
             // 
             // lblWachtwoord
             // 
             this.lblWachtwoord.AutoSize = true;
-            this.lblWachtwoord.Location = new System.Drawing.Point(58, 210);
+            this.lblWachtwoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWachtwoord.Location = new System.Drawing.Point(44, 171);
             this.lblWachtwoord.Name = "lblWachtwoord";
-            this.lblWachtwoord.Size = new System.Drawing.Size(86, 17);
+            this.lblWachtwoord.Size = new System.Drawing.Size(92, 24);
             this.lblWachtwoord.TabIndex = 1;
-            this.lblWachtwoord.Text = "Wachtwoord";
+            this.lblWachtwoord.Text = "Password";
             // 
             // tbGebruikersnaam
             // 
-            this.tbGebruikersnaam.Location = new System.Drawing.Point(61, 177);
+            this.tbGebruikersnaam.Location = new System.Drawing.Point(48, 146);
             this.tbGebruikersnaam.Name = "tbGebruikersnaam";
-            this.tbGebruikersnaam.Size = new System.Drawing.Size(129, 22);
+            this.tbGebruikersnaam.Size = new System.Drawing.Size(152, 22);
             this.tbGebruikersnaam.TabIndex = 2;
             // 
             // tbWachtwoord
             // 
-            this.tbWachtwoord.Location = new System.Drawing.Point(61, 230);
+            this.tbWachtwoord.Location = new System.Drawing.Point(48, 201);
             this.tbWachtwoord.Name = "tbWachtwoord";
             this.tbWachtwoord.PasswordChar = '*';
-            this.tbWachtwoord.Size = new System.Drawing.Size(129, 22);
+            this.tbWachtwoord.Size = new System.Drawing.Size(153, 22);
             this.tbWachtwoord.TabIndex = 3;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(60, 270);
+            this.btnLogin.Location = new System.Drawing.Point(48, 229);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(130, 31);
+            this.btnLogin.Size = new System.Drawing.Size(155, 38);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -85,16 +88,27 @@
             this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
             this.pbLogo.Location = new System.Drawing.Point(12, 12);
             this.pbLogo.Name = "pbLogo";
-            this.pbLogo.Size = new System.Drawing.Size(237, 132);
+            this.pbLogo.Size = new System.Drawing.Size(255, 95);
             this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbLogo.TabIndex = 5;
             this.pbLogo.TabStop = false;
+            // 
+            // chbRemember
+            // 
+            this.chbRemember.AutoSize = true;
+            this.chbRemember.Location = new System.Drawing.Point(69, 273);
+            this.chbRemember.Name = "chbRemember";
+            this.chbRemember.Size = new System.Drawing.Size(99, 21);
+            this.chbRemember.TabIndex = 6;
+            this.chbRemember.Text = "Remember";
+            this.chbRemember.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(279, 328);
+            this.ClientSize = new System.Drawing.Size(276, 310);
+            this.Controls.Add(this.chbRemember);
             this.Controls.Add(this.pbLogo);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbWachtwoord);
@@ -119,6 +133,7 @@
         private System.Windows.Forms.TextBox tbWachtwoord;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.CheckBox chbRemember;
     }
 }
 
--- 4Rails-2/4Rails-2/Login.cs
+++ 4Rails-2/4Rails-2/Login.cs
@@ -12,10 +12,17 @@ namespace _4Rails_2
 {
     public partial class Login : Form
     {
+        private string Username;
+        private string Password;
+        private bool Remember;
+
         public Login()
         {
             InitializeComponent();
+            
         }
+
+        
 
         private void Login_Load(object sender, EventArgs e)
         {
