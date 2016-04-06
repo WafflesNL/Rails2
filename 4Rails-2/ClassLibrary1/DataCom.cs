ac30bd0107fbb5f3a1838d710a9d96edf3eba3d8
Verhoeven,Maurick M.H.J. <maurick.verhoeven@student.fontys.nl>
2016-04-06 10:46:22 +0200

    datacom

---
+++ 4Rails-2/ClassLibrary1/DataCom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class DataCom
+    {
+        static void ConnectToDB()
+        {
+
+        }
+
+        static void Insert()
+        {
+
+        }
+
+        static string Read()
+        {
+            return "";
+        }
+
+        static void Remove()
+        {
+
+        }
+    }
+}
