cb0635a94776b23ca26ab5dafba06ca04f2a8703
kevin hofstede <kevin_hofstede@live.nl>
2016-04-13 11:15:52 +0200

    aaa

--- 4Rails-2/4Rails-2/Form1.Designer.cs
+++ 4Rails-2/4Rails-2/Form1.Designer.cs
@@ -28,36 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblGay = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblGay
-            // 
-            this.lblGay.AutoSize = true;
-            this.lblGay.Location = new System.Drawing.Point(140, 114);
-            this.lblGay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblGay.Name = "lblGay";
-            this.lblGay.Size = new System.Drawing.Size(179, 13);
-            this.lblGay.TabIndex = 0;
-            this.lblGay.Text = "Kevin houdt van Piemels in zijn anus";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(546, 314);
-            this.Controls.Add(this.lblGay);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(728, 386);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblGay;
     }
 }
 
